532bb00394060fb4273024f6dc61179316289b5c
Anna Calini <calinianna@gmail.com>
2018-01-13 18:40:51 +0000

    automatically backed up by learn

--- closure.js
+++ closure.js
@@ -1,8 +1,8 @@
 function parent() {
   var innerVariable = "Go to your room!";
-  console.log(inaccessible)
-  return function childe() {
-    var inaccessible = "**sneaks out the bedroom window**";
+  //
+  var childFunction = parent();
+  return function child() {
     return innerVariable;
   }
 }
